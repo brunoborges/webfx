7e66ddffc7ee069d24728dce3759e0482c069f2f
tarrsalah <tarrsalah@unix>
2012-12-16 15:10:37 +0100

    Add  FXML fileChooser input in the Menu

--- src/webfx/BrowserFXController.java
+++ src/webfx/BrowserFXController.java
@@ -4,8 +4,8 @@
  */
 package webfx;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -15,12 +15,12 @@ import javafx.collections.ListChangeListener.Change;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
 /**
  *
@@ -49,17 +49,34 @@ public class BrowserFXController implements Initializable {
         if (tabPane.getTabs().size() > 1) {
             tabPane.getTabs().remove(selectedTab.getSelectedIndex());
         }
+                
     }
+    
+    public void getFXML()  {
+        try {
+            FileChooser chooser = new FileChooser();                  
+            File selectedFile =chooser.showOpenDialog(null);
+            FXMLLoader loader=new FXMLLoader();            
+            Parent root = loader.load(getClass().getResource(selectedFile.getName()));
+            selectedTab.getSelectedItem().setContent(root);
+        } catch (IOException ex) {
+            Logger.getLogger(BrowserFXController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
 
     public void openFXPage() {
         try {
+                    
             String sUrl = urlField.getText();
 
             URL url = getClass().getResource(sUrl);
 
             FXMLLoader loader = new FXMLLoader();
             Parent root = loader.load(url);
-            
+
             selectedTab.getSelectedItem().setContent(root);
         } catch (IOException ex) {
             LOGGER.log(Level.SEVERE, null, ex);
@@ -77,7 +94,7 @@ public class BrowserFXController implements Initializable {
                 closeTab.disableProperty().set(change.getList().size() == 1);
             }
         });
-        
+
         selectedTab = tabPane.selectionModelProperty().getValue();
     }
 }
