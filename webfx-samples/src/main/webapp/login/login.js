e5109667a6788aa960dbec16191a54c4fdc74f69
Bruno Borges <bruno.borges@gmail.com>
2014-05-12 17:02:22 -0300

    Small improvements at login sample

--- webfx-samples/src/main/webapp/login/login.js
+++ webfx-samples/src/main/webapp/login/login.js
@@ -1,15 +1,15 @@
 var jString = Packages.java.lang.String;
 var $webfx = {title: "%title"};
 
-function handleSubmitButtonAction() {
-    var user = usernameField.getText();
-
-    var welcomeMessage = $webfx.i18n.getString("welcomeMessage");
-    welcomeMessage = jString.format(welcomeMessage, user);
-    actiontarget.setText(welcomeMessage);
-
+function doLogin() {
     // example of the NavigationContext feature
     if (user === "metronome") {
         $webfx.navigation.goTo("../metronome/metronome.fxml");
+    } else {
+        var user = usernameField.getText();
+
+        var welcomeMessage = $webfx.i18n.getString("unknowUserMessage");
+        welcomeMessage = jString.format(welcomeMessage, user);
+        actiontarget.setText(welcomeMessage);
     }
 }
