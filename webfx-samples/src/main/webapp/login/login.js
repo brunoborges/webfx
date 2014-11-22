1756a503056241797fda89f38b9844dfa2a28fde
Bruno Borges <bruno.borges@oracle.com>
2014-11-22 02:20:22 -0200

    username must come before

--- webfx-samples/src/main/webapp/login/login.js
+++ webfx-samples/src/main/webapp/login/login.js
@@ -3,11 +3,10 @@ var $webfx = {title: "%title"};
 
 function doLogin() {
     // example of the NavigationContext feature
+    var user = usernameField.getText();
     if (user === "metronome") {
         $webfx.navigation.goTo("../metronome/metronome.fxml");
     } else {
-        var user = usernameField.getText();
-
         var welcomeMessage = $webfx.i18n.getString("unknowUserMessage");
         welcomeMessage = jString.format(welcomeMessage, user);
         actiontarget.setText(welcomeMessage);
