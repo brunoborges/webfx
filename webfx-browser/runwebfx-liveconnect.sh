803e92b147cbad9ab8dae32da6d486fce72fa166
Bruno Borges <bruno.borges@oracle.com>
2014-11-23 20:02:59 -0200

    license review

---
+++ webfx-browser/runwebfx-liveconnect.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+CP=/home/bruno/NetBeansProjects/LiveConnectSample/dist/LiveConnectSample.jar:/home/bruno/Work/workspaces/javafx/webfx/webfx-browser/target/webfx-browser-1.0-SNAPSHOT.jar
+
+java -cp $CP webfx.browser.WebFX
