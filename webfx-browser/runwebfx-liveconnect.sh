f270d1650679e41efbfe479bb42fb09ebed0632e
Bruno Borges <bruno.borges@gmail.com>
2016-01-22 15:19:28 -0800

    removed unused shell script

--- webfx-browser/runwebfx-liveconnect.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-CP=/home/bruno/NetBeansProjects/LiveConnectSample/dist/LiveConnectSample.jar:/home/bruno/Work/workspaces/javafx/webfx/webfx-browser/target/webfx-browser-1.0-SNAPSHOT.jar
-
-java -cp $CP webfx.browser.WebFX
